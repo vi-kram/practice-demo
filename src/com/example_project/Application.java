b40ab491f77bc8d921ef75cacf872da6889de413
vi-kram <vikramjha848206@gmail.com>
2026-03-18 10:18:05 +0530

    Initial commit

--- src/com/example_project/Application.java
+++ src/com/example_project/Application.java
@@ -1,5 +1,13 @@
 package com.example_project;
 
+
+
 public class Application {
+	
+	public static void main(String[] args) {
+		System.out.println("Git Project ");
+		
+		System.out.println("Login Feature");
+	}
 
 }
